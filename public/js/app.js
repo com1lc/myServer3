a337253b1b2a6be8ac575bfd4f65bab0651749b5
ParrToo <plato2h@gmail.com>
2014-11-05 02:33:15 +0700

    เพิ่มส่วนแสดงตาราง และดึงข้อมูลจาก app.js มาแสดงในตารางนั้น

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,8 @@
 angular.module("myApp", [])
-.controller('mainCtrl', function($scope){
-	$scope.name = 'ParrToo';
-})
+ .controller('mainCtrl', function($scope){
+	$scope.books = [
+		{title: 'Titan', price: 300},
+		{title: 'Kamo', price: 500},
+		{title: 'Elsa', price: 800}
+	];
+ })
