fff5e6849ca64415488305014a58570d9a45c7db
ParrToo <plato2h@gmail.com>
2014-11-05 02:43:01 +0700

    เปลี่ยนข้อมูลที่จะแสดงในตารางไปเก็บไว้ใน index.js แทน

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-var express = require('express')
-var app = express()
-
-app.use(express.static(__dirname + '/public'));
-
-var server = app.listen(3000, function () {
-
-  	console.log("server is running")
-
+ var express = require('express')
+ var app = express()
+ 
+ app.use(express.static(__dirname + '/public'));
+ 
+app.get('/api/book',function(req,res){
+	var books = [
+		{title: 'Titan', price: 300},
+		{title: 'Kamo', price: 500},
+		{title: 'Elsa', price: 800}
+	];
+	res.send(books);
 })
+
+ var server = app.listen(3000, function () {
+ 
+   	console.log("server is running")
+ 
+ })
--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,7 @@
 angular.module("myApp", [])
- .controller('mainCtrl', function($scope){
-	$scope.books = [
-		{title: 'Titan', price: 300},
-		{title: 'Kamo', price: 500},
-		{title: 'Elsa', price: 800}
-	];
+ .controller('mainCtrl', function($scope,$http){
+	$scope.books = [];
+	$http.get('/api/book').success(function(data){
+		$scope.books=data;
+	})
  })
